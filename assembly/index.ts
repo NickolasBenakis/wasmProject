16f3f6ced33a9fda1aeb64022d19d6564398a7d5
nickolasBenakis <nickolasbenele@gmail.com>
2021-03-16 20:48:39 +0200

    Include memory read, write

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,39 +1,39 @@
-// The entry file of your WebAssembly module.
-declare function log(n: string): void;
+// // The entry file of your WebAssembly module.
+// declare function log(n: string): void;
 
-export function fizzBuzz(item: i32): String | null {
+// export function fizzBuzz(item: i32): String | null {
 
-  if (item%15 === 0) {
-    return "fizzbuzz"
-  } else if (item%3 === 0) {
-    return "fizz"
-  } else if (item%5 === 0) {
-    return "buzz"
-  }
+//   if (item%15 === 0) {
+//     return "fizzbuzz"
+//   } else if (item%3 === 0) {
+//     return "fizz"
+//   } else if (item%5 === 0) {
+//     return "buzz"
+//   }
 
-  return null;
+//   return null;
 
-}
-
-
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
-export function minusOne(a: i32): i32 {
-
-  if (a === 42)  {
-    abort();
-  }
-
-  return a - 1;
-}
-
-// memory.grow(2);
-
-// store<u8>(0, 21);
-// store<u8>(1, 99);
-
-// export function readMemory(n: i32): i32 {
-//   return load<u8>(n);
 // }
+
+
+// export function add(a: i32, b: i32): i32 {
+//   return a + b;
+// }
+
+// export function minusOne(a: i32): i32 {
+
+//   if (a === 42)  {
+//     abort();
+//   }
+
+//   return a - 1;
+// }
+
+memory.grow(2);
+
+store<u8>(0, 21);
+store<u8>(1, 99);
+
+export function readMemory(n: i32): i32 {
+  return load<u8>(n);
+}
--- js/app.js
+++ js/app.js
@@ -7,11 +7,19 @@ window.addEventListener('load', () => {
 	const ML = new WasmLoader();
 	ML.wasm('/build/optimized.wasm')
 	.then(instance => {
-		const {minusOne, fizzBuzz, __getString} = instance;
+		// const {minusOne, fizzBuzz, __getString} = instance;
 
+		const {readMemory, memory} = instance;
 		// document.write(minusOne(422));
-		document.write(__getString(fizzBuzz(3)));
+		// document.write(__getString(fizzBuzz(3)));
 
+		const memoryArray = new Uint8Array(memory.buffer);
+
+		document.write(memoryArray[1]);
+		document.write('<br />');
+		memoryArray[2] = 42;
+
+		document.write(readMemory(2));
 	})
 
 });
